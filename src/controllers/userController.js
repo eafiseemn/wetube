a5504150d9a1d1add4beb23567e10fee94b7986a
EUNJIN <eafiseemn@gmail.com>
2026-04-07 20:22:34 +0900

    Create Account + Hash Password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,51 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Create Account" });
+export const postJoin = async (req, res) => {
+	const { username, email, password, passwordConfirm, location } = req.body;
+	const pageTitle = "Create Account";
+	// Password Confirm
+	if (password !== passwordConfirm) {
+		return res.status(400).render("join", {
+			pageTitle,
+			errMsg: "Passwords do not match. Please try again.",
+			oldData: {
+				username,
+				email,
+				location,
+			},
+		});
+	}
+	try {
+		// Check for Duplicate Account
+		const userExists = await User.findOne({ $or: [{ email }, { username }] }).select(
+			"email username",
+		);
+		if (userExists) {
+			const errMsg =
+				userExists.email === email
+					? "This email is already registered."
+					: "This username is already taken.";
+			return res.status(400).render("join", {
+				pageTitle,
+				errMsg,
+				oldData: {
+					username,
+					email,
+					location,
+				},
+			});
+		}
+		// Create Account
+		await User.create({ username, email, password, location });
+		return res.redirect("/login");
+	} catch (err) {
+		console.error("Join Error: ", err);
+		return res
+			.status(500)
+			.render("join", { pageTitle, errMsg: "Internal Server Error. Please try again later." });
+	}
+};
 export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log Out");
 export const profile = (req, res) => res.send("See Profile");
