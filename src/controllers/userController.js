209be2d342840d418afefd8e9c8b3d2c0560d84e
EUNJIN <eafiseemn@gmail.com>
2026-04-15 15:52:27 +0900

    Social Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
 
+/************** Join **************/
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Create Account" });
 export const postJoin = async (req, res) => {
 	const { username, email, password, passwordConfirm, location } = req.body;
@@ -47,6 +49,9 @@ export const postJoin = async (req, res) => {
 			.render("join", { pageTitle, errorMsg: "Internal Server Error. Please try again later." });
 	}
 };
+
+/************** LogIn **************/
+
 export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
 export const postLogin = async (req, res) => {
 	const { username, password } = req.body;
@@ -68,7 +73,7 @@ export const postLogin = async (req, res) => {
 		}
 		// Login Success
 		req.session.loggedIn = true;
-		req.session.user = user;
+		req.session.user = user.toObject();
 		return res.redirect("/");
 	} catch (err) {
 		console.error("Login Error: ", err);
@@ -77,7 +82,97 @@ export const postLogin = async (req, res) => {
 			.render("login", { pageTitle, errorMsg: "Internal Server Error. Please try again later." });
 	}
 };
-export const logout = (req, res) => res.send("Log Out");
+
+/************** Social Login (GitHub) **************/
+
+export const startGithubLogin = (req, res) => {
+	// Redirect to GitHub
+	const baseUrl = "https://github.com/login/oauth/authorize";
+	const config = {
+		client_id: process.env.GITHUB_CLIENT,
+		scope: "read:user user:email",
+		allow_signup: false,
+	};
+	const params = new URLSearchParams(config).toString();
+	const finalUrl = baseUrl + "?" + params;
+	return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+	try {
+		// request access token
+		const baseUrl = "https://github.com/login/oauth/access_token";
+		const config = {
+			client_id: process.env.GITHUB_CLIENT,
+			client_secret: process.env.GITHUB_SECRET,
+			code: req.query.code,
+		};
+		const params = new URLSearchParams(config).toString();
+		const finalUrl = baseUrl + "?" + params;
+		const data = await fetch(finalUrl, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+			},
+		});
+		const tokenRequest = await data.json();
+
+		// fetch user information
+		if ("access_token" in tokenRequest) {
+			const accessToken = tokenRequest["access_token"];
+			const apiUrl = "https://api.github.com/user";
+			const headers = {
+				Authorization: `token ${accessToken}`,
+			};
+			const userData = await (await fetch(apiUrl, { headers })).json();
+			const emailData = await (await fetch(`${apiUrl}/emails`, { headers })).json();
+			const emailObj = emailData.find((email) => email.primary === true && email.verified === true);
+
+			if (!emailObj) {
+				return res.status(400).render("login", {
+					pageTitle,
+					errorMsg: "Can't verify Email connected to your GitHub Account.",
+				});
+			}
+
+			// Check if there's user having a same email account
+			let user = await User.findOne({ email: emailObj.email });
+			if (!user) {
+				// Create new User if there's no registered user
+				user = await User.create({
+					username: userData.login,
+					email: emailObj.email,
+					password: "",
+					avatarUrl: userData.avatar_url,
+					socialOnly: true,
+					location: userData.location,
+				});
+			}
+			req.session.loggedIn = true;
+			req.session.user = user.toObject();
+			return res.redirect("/");
+		} else {
+			console.error("Github Token Error");
+			return res.status(500).render("login", {
+				pageTitle: "Login",
+				errorMsg: "Internal Server Error. Please try again later.",
+			});
+		}
+	} catch (err) {
+		console.error("Github Login Error", err);
+		return res.status(500).render("login", {
+			pageTitle: "Login",
+			errorMsg: "Internal Server Error. Please try again later.",
+		});
+	}
+};
+
+/************** Log Out **************/
+export const logout = (req, res) => {
+	req.session.destroy();
+	return res.redirect("/");
+};
+
 export const profile = (req, res) => res.send("See Profile");
 export const edit = (req, res) => res.send("Edit Profile");
 export const remove = (req, res) => res.send("Delete Profile");
--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,9 @@ import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true },
 	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+	password: String,
+	avatarUrl: String,
+	socialOnly: { type: Boolean, required: true, default: false },
 	location: String,
 });
 
